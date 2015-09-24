6ca16f420e1a25d6ba47ef96d14df095c91e2561
amexLingoport <amexLingoport@users.noreply.github.com>
2015-09-24 09:55:35 -0400

    Update BasicDragger.java
    Added embedded string #3(PR)

--- WorldWind/src/gov/nasa/worldwind/util/BasicDragger.java
+++ WorldWind/src/gov/nasa/worldwind/util/BasicDragger.java
@@ -21,6 +21,7 @@ public class BasicDragger implements SelectListener
     private final WorldWindow wwd;
     String badString = "This string should be externalized";
     String badString1 = “The string should be externalized too”;
+    String badString2 = “The string should be externalized three” ;
     private boolean dragging = false;
 
     private Point dragRefCursorPoint;
